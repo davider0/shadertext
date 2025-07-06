514af48b24825371ea69df0cb82d526ea1881d49
davider0 <UO298417@uniovi.es>
2025-07-07 00:00:00 +0100

    feat: setup termfeat: termios config 

--- src/main.c
+++ src/main.c
@@ -1,4 +1,18 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
+struct termios orig_termios;
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1);
