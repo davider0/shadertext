cc8f3066fa2add985796c2c4b4eeeb0a4f4c9b36
davider0 <UO298417@uniovi.es>
2025-07-19 21:18:59 +0200

    fix: build_mscv.bat

--- build_msvc.bat
+++ build_msvc.bat
@@ -2,7 +2,7 @@
 rem launch this from msvc-enabled console
 
 set CFLAGS=/W4 /WX /wd4244 /wd4305 /wd4018 /wd4702 /std:c11 /wd4996 /wd5105 /wd4200 /wd4013 /Qspectre- /FC /TC /Zi /nologo /EHsc /MD 
-set INCLUDES=/I dependencies\SDL2\include /I dependencies\GLEW\include /I dependencies\freetype\include /I dependencies\freetype\include\config
+set INCLUDES=/I dependencies\SDL2\include /I dependencies\GLEW\include /I dependencies\freetype\include /I dependencies\freetype\include\freetype
 set LIBS=dependencies\SDL2\lib\x64\SDL2.lib ^
          dependencies\SDL2\lib\x64\SDL2main.lib ^
          dependencies\GLFW\lib\glfw3.lib ^
