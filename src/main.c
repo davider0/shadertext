c32a05d69e5f083aee67849aabba6968cc9f3258
davider0 <UO298417@uniovi.es>
2025-07-29 17:43:16 +0100

    feat: display of number of tildes on screen

--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@
 
 /*** data ***/
 struct editorConfig {
-      int screenrows;
-  int screencols;
+    int screenrows;
+    int screencols;
   struct termios orig_termios;
 };
 struct editorConfig E;
@@ -68,7 +68,7 @@ void enableRawMode() {
 
   void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
@@ -91,9 +91,12 @@ void enableRawMode() {
   }
   
 /*** init ***/
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
 int main(){
-
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
